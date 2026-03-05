5d044b009d0a9cf729ab9b8790a5684e40fc6352
nadh <nrlndh04@gmail.com>
2026-03-06 00:20:34 +0800

    ipaddress

--- lib/myconfig.dart
+++ lib/myconfig.dart
@@ -1,4 +1,4 @@
 class MyConfig {
-  static const String server = 'http://192.168.1.15';
+  static const String server = 'http://192.168.1.8';
   static const String apiUrl = '$server/school_api';
 }
